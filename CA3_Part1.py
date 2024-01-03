3ef1e2ef27d24b10baa8e7af4781af7992204f79
Diego Alexander <114578666+Dm2998@users.noreply.github.com>
2024-01-03 22:19:29 +0000

    Update CA3_Part1.py

--- CA3_Part1.py
+++ CA3_Part1.py
@@ -6,11 +6,11 @@ from selenium.webdriver.chrome.service import Service
 
 serv_obj = Service("C:\drivers\chromedriver.exe")           # Create a new instance of the Chrome driver
 driver = webdriver.Chrome(service=serv_obj)
-driver.get("https://www.ebay.com/")           # search for a ebay website
-driver.maximize_window()                      # maximize the window
-print(driver.title)                           # print the title of the page
-time.sleep(10)                                   # wait for 10 seconds
-driver.quit()                                    # close the browser
+driver.get("https://www.ebay.com/")           # search for a ebay website.
+driver.maximize_window()                      # maximize the window.
+print(driver.title)                           # print the title of the page.
+time.sleep(10)                                   # wait for 10 seconds.
+driver.quit()                                    # close the browser.
 
 
 
